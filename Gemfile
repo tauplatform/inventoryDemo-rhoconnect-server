22e572ee64d5123b256376722d44ae83fa4dfe82
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2017-03-03 01:17:03 +0300

    migrate to Amazon S3 serive for store blobs

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rhoconnect'
+gem 'aws-sdk'
 
 gemfile_path = File.join(File.dirname(__FILE__), ".rcgemfile")
 begin
--- models/ruby/inventory_item.rb
+++ models/ruby/inventory_item.rb
@@ -1,25 +1,44 @@
 require 'json'
 require 'rest_client'
+require 'aws-sdk'
 
 class InventoryItem < Rhoconnect::Model::Base
 
+
   def initialize(source)
     @base = 'http://taustore.herokuapp.com/inventory_items'
+
+
+
+    #for Amazon S3 service you must define next three environment variables (or you should define it programatically):
+    # export AWS_ACCESS_KEY_ID=SHSKJHS_example
+    # export AWS_SECRET_ACCESS_KEY=KJHGKJHGKJHGKJHGKJHG_example
+    # export AWS_DEFAULT_REGION=us-west-2
+    @s3 = Aws::S3::Client.new()
+
+
+    #@s3 = Aws::S3::Client.new(
+    #  access_key_id: 'SHSKJHS_example',
+    #  secret_access_key: 'KJHGKJHGKJHGKJHGKJHG_example',
+    #  region: 'us-west-2'
+    #)
+    @bucket = Aws::S3::Bucket.new('inventory-demo', {:client => @s3})
     super(source)
   end
 
   def query(params=nil)
+    puts "Query: #{params.to_s}"
     parsed = JSON.parse(RestClient.get("#{@base}.json").body)
-
     @result={}
     parsed.each do |item|
       @result[item["inventory_item"]["id"].to_s] = item["inventory_item"]
     end if parsed
+    @result
   end
 
   def create(create_hash)
+    puts "Create: #{create_hash}"
     res = RestClient.post(@base, :inventory_item => create_hash)
-
     # After create we are redirected to the new record.
     # We need to get the id of that record and return
     # it as part of create so rhosync can establish a link
@@ -31,9 +50,7 @@ class InventoryItem < Rhoconnect::Model::Base
   end
 
   def update(update_hash)
-
     puts "Update: #{update_hash}"
-
     obj_id = update_hash['id']
     update_hash.delete('id')
     RestClient.put("#{@base}/#{obj_id}", :inventory_item => update_hash)
@@ -44,21 +61,19 @@ class InventoryItem < Rhoconnect::Model::Base
   end
 
   def store_blob(obj, field_name, blob)
-    puts "store blob for field #{field_name}"
+    puts "Store blob for field [#{field_name}] blob[#{blob.to_s}]"
+
     extension = File.extname(blob[:filename])
-    filename = "#{SecureRandom.uuid}#{extension}"
-    url = 'http://taustore.herokuapp.com/upload'
+    key = "#{SecureRandom.uuid}#{extension}"
 
-    response = RestClient.post(url, {
-        :file => File.new(blob[:tempfile].path, 'rb'),
-        :filename => filename,
-        :accept => :json
-    })
+    object = @bucket.object(key)
 
-    json = JSON.parse(response.body)
+    object.upload_file(blob[:tempfile].path, {:acl => 'public-read'})
 
-    puts "Filename on server: #{json['filename']}"
+    public_url = object.public_url
 
-    json['filename']
+    puts "Public URL on server: #{public_url}"
+
+    public_url
   end
 end
